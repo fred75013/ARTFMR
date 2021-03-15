83c511d295991ee2b9991964b10a56e1fd8dad60
meltek13 <regnault.melvin@hotmail.fr>
2021-03-15 10:08:20 +0100

    Upadte: add 3 column to product : to_rent, to_buy, status

--- db/migrate/20210314143438_create_products.rb
+++ db/migrate/20210314143438_create_products.rb
@@ -4,13 +4,16 @@ class CreateProducts < ActiveRecord::Migration[6.1]
       t.string :title
       t.string :description
       t.string :category
-      t.string :type
+      t.string :style
       t.integer :height
       t.integer :width
       t.integer :depth
       t.integer :weight
       t.boolean :border_product
       t.boolean :authenticable_product
+      t.string :status
+      t.boolean :to_rent
+      t.boolean :to_buy
       t.integer :price
       t.string :image_url
 
