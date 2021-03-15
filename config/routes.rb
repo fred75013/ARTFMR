b9e29018a08daf526fd5e7c2bc81f2b0b5261ab1
Theo CAZENAVE <theocazenavecoupet@gmail.com>
2021-03-15 12:52:43 +0100

    ADD: root to buy with sttripe

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  root "static_page#index"
- 
+  root "static_page#index" 
   get 'carts/show'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -9,5 +8,6 @@ Rails.application.routes.draw do
   resources :line_products
   resources :carts
 
+   resources :checkout, only: [:create]
 
 end
