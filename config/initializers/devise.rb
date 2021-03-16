abb09277be137428452b46d66bfb001f1ac74c32
Theo CAZENAVE <theocazenavecoupet@gmail.com>
2021-03-16 14:30:29 +0100

    ADD: edit user in show user

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,7 @@ Devise.setup do |config|
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
   # config.secret_key = 'aaef7faa2559b2a89c337c78032a2d9db1b65dd11506012bcab76e059d8cb9c26f5b8afde9536f4db09c55cddad7b64068940e9e36c54bd8573cdb60ff74451a'
-
+  
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
@@ -244,7 +244,7 @@ Devise.setup do |config|
   # Turn scoped views on. Before rendering "sessions/new", it will first check for
   # "users/sessions/new". It's turned off by default because it's slower if you
   # are using only default views.
-  # config.scoped_views = false
+  config.scoped_views = true
 
   # Configure the default scope given to Warden. By default it's the first
   # devise role declared in your routes (usually :user).
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_121856) do
+ActiveRecord::Schema.define(version: 2021_03_16_130626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,8 +51,6 @@ ActiveRecord::Schema.define(version: 2021_03_15_121856) do
   create_table "line_products", force: :cascade do |t|
     t.integer "product_id"
     t.integer "cart_id"
-    t.boolean "to_buy", default: true
-    t.integer "price"
     t.integer "quantity"
     t.integer "order_id"
     t.datetime "created_at", precision: 6, null: false
@@ -71,6 +69,8 @@ ActiveRecord::Schema.define(version: 2021_03_15_121856) do
     t.boolean "border_product"
     t.boolean "authenticable_product"
     t.string "status"
+    t.boolean "to_rent"
+    t.boolean "to_buy"
     t.integer "price"
     t.string "image_url"
     t.datetime "created_at", precision: 6, null: false
@@ -85,6 +85,12 @@ ActiveRecord::Schema.define(version: 2021_03_15_121856) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "adress"
+    t.string "city"
+    t.integer "phone_number"
+    t.boolean "artist"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
