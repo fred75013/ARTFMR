32ad24b651dd6c63a8ef55bac761e79a121eee19
frederic <fredericseguier@yahoo.fr>
2021-03-16 12:55:39 +0100

    UPDATE: fix bug action mailer sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-config.action_mailer.perform_deliveries = true
+config.action_mailer.perform_deliveries = false
 config.action_mailer.default_url_options = { :host => 'https://artfmr.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
