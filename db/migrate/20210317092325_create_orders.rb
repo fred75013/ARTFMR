1e1391978a1111305bb1348651001587942c73be
meltek13 <regnault.melvin@hotmail.fr>
2021-03-17 14:25:33 +0100

    ADD: table joint Order_product + condition for change status product to rented or sold

--- db/migrate/20210317092325_create_orders.rb
+++ db/migrate/20210317092325_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.belongs_to :user, index: true
       t.belongs_to :cart
       t.integer :amount
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_092325) do
+ActiveRecord::Schema.define(version: 2021_03_17_111100) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,12 +61,25 @@ ActiveRecord::Schema.define(version: 2021_03_17_092325) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "order_products", force: :cascade do |t|
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
+  end
+
   create_table "orders", force: :cascade do |t|
+    t.bigint "user_id"
     t.bigint "cart_id"
     t.integer "amount"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["cart_id"], name: "index_orders_on_cart_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
