a9a75cdda2b55bcb9920889365715dd6422c8599
Theo CAZENAVE <theocazenavecoupet@gmail.com>
2021-03-22 12:57:00 +0100

    ADD: nickname to artist

--- db/migrate/20210316130626_edit_users.rb
+++ db/migrate/20210316130626_edit_users.rb
@@ -2,6 +2,7 @@
 
 class EditUsers < ActiveRecord::Migration[6.1]
   def change
+    add_column :users, :nickname, :string
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
     add_column :users, :adress, :string
