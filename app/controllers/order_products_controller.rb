0fae7be8fcf218b41684958cdfccae0e56b8b313
frederic <fredericseguier@yahoo.fr>
2021-03-23 11:46:29 +0100

    UPDATE: profil page

--- app/controllers/order_products_controller.rb
+++ app/controllers/order_products_controller.rb
@@ -1,17 +1,8 @@
 class OrderProductsController < ApplicationController
   def update
     @order_product = OrderProduct.find(params[:id])
-    if @order_product.order.user == current_user && @order_product.artist_confirmation != "validated"
-      @order_product.update(customer_confirmation: "validated")
-    end
-    if @order_product.product.admin == current_user && @order_product.order.user != "validated"
-      @order_product.update(artist_confirmation: "validated")
-    end
-    if @order_product.order.user == current_user && @order_product.artist_confirmation == "validated"
-      @order_product.update(start_date: Date.today, end_date: Date.today.advance(days: 30))
-    end
-    if @order_product.product.admin == current_user && @order_product.order.user == "validated"
-      @order_product.update(start_date: Date.today, end_date: Date.today.advance(days: 30))
+    if @order_product.artist_confirmation != "validated"
+      @order_product.update(artist_confirmation: "validated", start_date: Date.today, end_date: Date.today.advance(days: 30))
     end
     redirect_to request.referrer
   end
